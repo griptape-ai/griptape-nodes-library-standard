e4c1e7df50512ae9fca727138776f077d0712640
Andrew French <andrew@afren.ch>
2026-04-13 14:53:24 -0700

    BYOK/import alignment

--- griptape_nodes_library/video/kling_identify_face.py
+++ griptape_nodes_library/video/kling_identify_face.py
@@ -13,7 +13,7 @@ from griptape_nodes.exe_types.param_types.parameter_string import ParameterStrin
 from griptape_nodes.exe_types.param_types.parameter_video import ParameterVideo
 from griptape_nodes.traits.options import Options
 
-from griptape_nodes_library.griptape_proxy_node import GriptapeProxyNode
+from griptape_nodes_library.proxy import GriptapeProxyNode
 
 logger = logging.getLogger("griptape_nodes")
 
