2329d43847d5346b1eda6c3eb89d8312d2001270
Jason Schleifer <jason.schleifer@gmail.com>
2026-06-27 08:21:38 +1200

    using regex

--- griptape_nodes_library/variables/create_variable.py
+++ griptape_nodes_library/variables/create_variable.py
@@ -344,26 +344,7 @@ class CreateVariable(ControlNode):
 
     @staticmethod
     def _has_file_extension(s: str) -> bool:
-        _EXTENSIONS = {
-            ".png",
-            ".jpg",
-            ".jpeg",
-            ".gif",
-            ".webp",
-            ".bmp",
-            ".mp4",
-            ".mov",
-            ".avi",
-            ".mp3",
-            ".wav",
-            ".ogg",
-            ".json",
-            ".txt",
-            ".csv",
-            ".pdf",
-        }
-        _, ext = os.path.splitext(s)
-        return ext.lower() in _EXTENSIONS
+        return bool(re.search(r"\.[a-zA-Z0-9]{2,5}$", s))
 
     def before_value_set(self, parameter: Parameter, value: Any) -> Any:
         """Handle changes to the variable_type parameter."""
